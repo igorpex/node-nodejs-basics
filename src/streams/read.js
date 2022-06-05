fbb9eebdadfa075d20363c8f86c71bfab2f6d17a
igorpex <igor.bogdanov@gmail.com>
2022-06-05 16:16:00 +0300

    feat: streams - read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,19 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { getDirname } from '../utils.js';
+const __dirname = getDirname(import.meta.url);
+
 export const read = async () => {
-    // Write your code here 
+    const { stdout } = process;
+    const file = path.join(__dirname, 'files', 'fileToRead.txt');
+    const stream = fs.createReadStream(file, 'utf-8');
+
+    stream.on('data', chunk => stdout.write(chunk));
+    stream.on('end', () => process.exit());
 };
+
+//test
+read();
+
+// read.js - implement function that reads file fileToRead.txt content
+// using Readable Stream and prints it's content into process.stdout
