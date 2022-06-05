93997556801c6faa0a2dc613f2b4b0b31d757e88
igorpex <igor.bogdanov@gmail.com>
2022-06-05 23:24:27 +0300

    feat: cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,21 @@
+import path from 'path';
+import { getDirname } from '../utils.js';
+const __dirname = getDirname(import.meta.url);
+import { fork } from 'child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const filePath = path.join(__dirname, 'files', 'script.js');
+    const child = fork(filePath, args, { stdio: ['pipe', 'pipe', 'pipe', 'ipc'] });
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
+    child.stdout.on('data',
+        (data) => {
+            console.info(`Received from child process: "${data.toString().trim()}"`);
+        })
 };
 
-spawnChildProcess();
+// test
+// from command line: "node cp.js --one --two"
+// from script: "npm run cp"
+const args = process.argv.slice(2);
+spawnChildProcess(args);
