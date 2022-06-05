69b537a16ccbeb5cb66f18e2d8c792a0a4698abd
igorpex <igor.bogdanov@gmail.com>
2022-06-05 16:16:00 +0300

    feat: streams - read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { getDirname } from '../utils.js';
+const __dirname = getDirname(import.meta.url);
+
 const read = async () => {
-    // Write your code here 
+    const { stdout } = process;
+    const file = path.join(__dirname, 'files', 'fileToRead.txt');
+    const stream = fs.createReadStream(file, 'utf-8');
+
+    stream.on('data', chunk => stdout.write(chunk));
+    stream.on('end', () => process.exit());
 };
 
 await read();
