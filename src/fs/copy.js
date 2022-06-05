efb20b64a50ea5476ec3c48ed9fc03274ba85058
igorpex <igor.bogdanov@gmail.com>
2022-06-05 16:14:13 +0300

    feat: update fs-copy with fs/promises.cp()

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -3,8 +3,69 @@ import * as path from 'path';
 import { getDirname } from '../utils.js';
 const __dirname = getDirname(import.meta.url);
 
-
+/**
+* copy.js - copies folder `srcDir` with all its content into folder `destDir`.
+* Defeult `srcDir` is `files` and `destDir` is `files_copy`.
+* If `srcDir` folder doesn't exists or `destDir` has already been created
+* Error with message `FS operation failed` is thrown
+ * @param {String} srcDir - should be source subdirectry(subfolder) name. Default is `files`
+ * @param {String} destDir - should be destination subdirectry(subfolder) name. Default is `files-copy`
+ */
 export const copy = async (srcDir, destDir) => {
+  // Write your code here 
+  if (!srcDir) srcDir = path.join(__dirname, 'files');
+  if (!destDir) destDir = path.join(__dirname, 'files-copy');
+
+  const checkDirsExist = async () => {
+
+    try {
+      let srcStat = await fs.stat(srcDir);
+      if (!srcStat.isDirectory()) {
+        throw new Error("FS operation failed - no src directory");
+      }
+    } catch (e) {
+      if (e.code === 'ENOENT') {
+        throw new Error("FS operation failed - no src directory");
+      }
+    }
+
+    try {
+      let destStat = await fs.stat(destDir);
+      if (destStat.isDirectory()) {
+        throw new Error("FS operation failed - dest directory exists")
+      }
+    } catch (e) {
+      if (e.code === 'ENOENT') {
+        // console.log('Error', e);
+      } else {
+        throw new Error("FS operation failed - dest directory exists");
+      }
+    }
+
+  }
+
+  const copyDir = async () => {
+    try {
+      fs.cp(srcDir, destDir, {
+        recursive: true,
+        preserveTimestamps: true,
+        // force: false,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+
+  }
+  await checkDirsExist();
+  copyDir();
+};
+
+/**
+* copyAlternate - alternate way to copy dir with subdirs without using fs/promises.cp()
+ * @param {String} srcDir - should be source subdirectry(subfolder) name. Default is `files`
+ * @param {String} destDir - should be destination subdirectry(subfolder) name. Default is `files-copy`
+ */
+export const copyAlternate = async (srcDir, destDir) => {
   // Write your code here 
   if (!srcDir) srcDir = path.join(__dirname, 'files');
   if (!destDir) destDir = path.join(__dirname, 'files-copy');
@@ -81,3 +142,6 @@ export const copy = async (srcDir, destDir) => {
   };
   copyDir(srcDir, destDir);
 };
+
+//test
+copy();
