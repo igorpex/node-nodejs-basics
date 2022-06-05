7fe07d60f87916f72e43d2504ea9e5b825d58eed
igorpex <igor.bogdanov@gmail.com>
2022-06-05 15:33:13 +0300

    feat: cli-args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,22 @@
 const parseArgs = () => {
-    // Write your code here 
-};
+    const args = process.argv.slice(2);
 
+    // prepate props object
+    let props = {};
+    args.forEach((element, index, array) => {
+        if (element.startsWith('--')) {
+            const propName = element.slice(2);
+            const propValue = array[index + 1];
+            props[propName] = propValue;
+        }
+    })
+
+    // prepare string and print it
+    let textArr = [];
+    for (let propName in props) {
+        let text = `${propName} is ${props[propName]}`
+        textArr.push(text);
+    }
+    console.log(textArr.join(', '));
+};
 parseArgs();
