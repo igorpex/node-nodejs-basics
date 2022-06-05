0f9b2274121d21bdb656ddca8e3290cfc95b4bd2
igorpex <igor.bogdanov@gmail.com>
2022-06-05 16:10:01 +0300

    feat: hash-calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,31 @@
-export const calculateHash = async () => {
-    // Write your code here 
+import fs from 'fs/promises';
+import path from 'path';
+import crypto from 'crypto';
+import { getDirname } from '../utils.js';
+const __dirname = getDirname(import.meta.url);
+
+/**
+ * calculates SHA256 hash for the given file and return it as hex
+ * @param {String} fileName - should be filename in `files` subfolder. Default is `fileToCalculateHashFor.txt`
+ */
+export const calculateHash = async (fileName) => {
+    if (!fileName) fileName = 'fileToCalculateHashFor.txt';
+    const filePath = path.join(__dirname, 'files', fileName);
+    try {
+        const fileBuffer = await fs.readFile(filePath);
+        const hash = crypto.createHash('sha256');
+        hash.update(fileBuffer);
+        const hexHash = hash.digest('hex');
+        return hexHash;
+    } catch (e) {
+        if (e.code === 'ENOENT') {
+            throw new Error("FS operation failed - file not found");
+        } else {
+            console.log(e);
+        }
+    }
 };
+
+// test
+console.log(await calculateHash());
+
