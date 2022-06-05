bb4a8641b161f3e2b5a6a353a397a4293208c675
igorpex <igor.bogdanov@gmail.com>
2022-06-05 16:16:30 +0300

    feat: streams - transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,29 @@
 export const transform = async () => {
-    // Write your code here 
+    // Write your code here
+    const { stdin, stdout } = process;
+
+    stdout.write('Введите текст для трансформации. Введите "exit" или нажмите Ctrl+C для окончания.\n');
+
+    stdin.on('data', chunk => {
+        if (chunk.toString().trim() !== 'exit') {
+            const transformedChunk = chunk.toString().trim().split('').reverse().join('');
+            stdout.write(transformedChunk + '\n');
+            stdout.write('Еще введите текст для трансформации. Введите "exit" или нажмите Ctrl+C для окончания.\n');
+
+        } else {
+            handleExit();
+        }
+    });
+
+    function handleExit() {
+        stdout.write('\nТрансформация строк окончена. Выход.\n');
+        process.exit();
+    }
+
+    process.on('SIGINT', () => handleExit());  // CTRL+C
 };
+
+//test
+transform();
+//transform.js - implement function that reads data from process.stdin,
+// reverses text using Transform Stream and then writes it into process.stdout
