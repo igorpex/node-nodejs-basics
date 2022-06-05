60798602620e4841cda1b0d09517ba923559705c
igorpex <igor.bogdanov@gmail.com>
2022-06-05 16:16:16 +0300

    feat: streams - write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,35 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { getDirname } from '../utils.js';
+const __dirname = getDirname(import.meta.url);
+
 export const write = async () => {
-    // Write your code here 
+
+    const file = path.join(__dirname, 'files', 'fileToWrite.txt');
+    const output = fs.createWriteStream(file, 'utf-8');
+
+    const { stdin, stdout } = process;
+
+    stdout.write('Введите текст для записи в файл. Введите "exit" или нажмите Ctrl+C для окончания записи.\n');
+
+    stdin.on('data', chunk => {
+        if (chunk.toString().trim() !== 'exit') {
+            output.write(chunk);
+        } else {
+            handleExit();
+        }
+    });
+
+    function handleExit() {
+        stdout.write('\nЗапись файла окончена. Выход.\n');
+        process.exit();
+    }
+
+    process.on('SIGINT', () => handleExit());  // CTRL+C
 };
+
+//test
+write();
+
+//write.js - implement function that writes
+// process.stdin data into file fileToWrite.txt content using Writable Stream
